fd99182de18b7d7b919aca3f5bcbc916284b6a71
CyberT33N <cybert33n@gmail.com>
2022-05-27 22:58:06 +0200

    fix(CCS-001): fixed .gitignore to ignore services/gitlab-runner/config

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
+echo 'docker-compose down init..'
 sudo docker-compose down
+echo '\n\n'
+
+echo 'docker-compose up init..'
 sudo docker-compose up -d
-sudo docker-compose exec gitlab-runner gitlab-runner register
+echo '\n\n'
+
+echo 'start gitlab-runner..'
+#sudo docker-compose exec gitlab-runner gitlab-runner register
+sudo docker-compose start gitlab-runner
