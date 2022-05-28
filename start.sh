dca41d1c4ba875dccce2d7f32047d090acc2f9df
CyberT33N <cybert33n@gmail.com>
2022-05-28 16:45:47 +0200

    fix(CCS-001): backup

--- start.sh
+++ start.sh
@@ -6,7 +6,18 @@ echo 'docker-compose up init..'
 sudo docker-compose up -d
 echo '\n\n'
 
-#echo 'start gitlab-runner..'
+# echo 'register gitlab-runner..'
+#sudo docker-compose exec gitlab-runner gitlab-runner register \
+#--non-interactive \
+#--url http://gitlab.local.com/ \
+#--tag-list "test" \
+#--registration-token xxxxxxxxxxxxx \
+#--executor docker \
+#--docker-image node:18.2.0 \
+#--docker-network-mode localdev
+
+# Alternative you can use the interactive mode:
+# sudo docker-compose exec gitlab-runner gitlab-runner register
+
 # sudo docker-compose exec gitlab-runner gitlab-runner list
-#sudo docker-compose exec gitlab-runner gitlab-runner register
-#sudo docker-compose start gitlab-runner
+# sudo docker-compose start gitlab-runner
