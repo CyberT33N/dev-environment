c0ebd209c03a2ebfbd0d786be7975639f4b63e8f
CyberT33N <cybert33n@gmail.com>
2022-06-23 17:22:56 +0200

    fix(CCS-001): metrics.interim not found at mongodb

--- start.sh
+++ start.sh
@@ -2,9 +2,15 @@ echo 'docker-compose down init..'
 sudo docker-compose down
 echo '\n\n'
 
+# If you get Error: find: '/data/db/diagnostic.data/metrics.interim': No such file or directory
+# We delete the folder diagnostic.data from our mounted volume and then mongo will re-create it
+# sudo rm -rf /srv/mongo/diagnostic.data
+
 echo 'docker-compose up init..'
 sudo docker-compose up -d
 echo '\n\n'
+
+## ------------------------------------------------------- ##
 
 #echo 'register gitlab-runner..'
 #sudo docker-compose exec gitlab-runner gitlab-runner register \
@@ -19,5 +25,7 @@ echo '\n\n'
 # Alternative you can use the interactive mode:
 # sudo docker-compose exec gitlab-runner gitlab-runner register
 
-# sudo docker-compose exec gitlab-runner gitlab-runner list
-# sudo docker-compose start gitlab-runner
+## ------------------------------------------------------- ##
+
+sudo docker-compose exec gitlab-runner gitlab-runner list
+sudo docker-compose start gitlab-runner
